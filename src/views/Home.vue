70587d54b0bb193505d2c712bedc09b158544835
jnprogrammer <joshuanparker@gmail.com>
2022-11-25 16:35:40 -0500

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
   }
 };
 </script>
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
